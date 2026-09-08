1f653f273b23f63eb77594b98d4df96c8f63e608
Comamoca <comamoca.dev@gmail.com>
2026-09-09 01:05:27 +0900

    feat(todo_app): Publish devcontainer image to GHCR
    Add a dockerImage package to todo_app's flake.nix and its own
    devcontainer.json pointing at ghcr.io/comamoca/gleam-by-example/todo_app,
    plus a root workflow that auto-discovers example directories (any dir
    with both a flake.nix and .devcontainer/devcontainer.json) and publishes
    each one's dockerImage to GHCR.

---
+++ todo_app/flake.nix
@@ -0,0 +1,175 @@
+{
+  description = "A basic flake to with Gleam language";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    devenv.url = "github:cachix/devenv";
+    gleam-overlay.url = "github:Comamoca/gleam-overlay";
+    gleam2nix.url = "git+https://git.isincredibly.gay/srxl/gleam2nix";
+    gleam2nix.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      systems,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+        inputs.devenv.flakeModule
+        inputs.process-compose-flake.flakeModule
+      ];
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
+	  
+          app = pkgs.buildGleamApplication {
+            pname = "hello";
+            version = "1.0.0";
+            src = pkgs.lib.cleanSource ./.;
+            gleamNix = import ./gleam.nix { inherit (pkgs) lib; };
+            gleam = pkgs.gleam.bin.latest;
+          };
+
+          erlangPackages = with pkgs.beamMinimal28Packages; [
+            erlang
+            rebar3
+
+            # elixir_1_19
+          ];
+
+          gleamPackages = with pkgs; [
+            gleam2nix
+            gleam.bin.latest
+          ];
+
+          devdeps-todo-app =
+            with pkgs;
+            [
+              nixd
+              just
+
+              atlas
+              sqlite
+              sqlitebrowser
+            ]
+            ++ erlangPackages
+            ++ gleamPackages;
+
+          devShellImage = pkgs.dockerTools.buildImage {
+            name = "devShellImage";
+            fromImage = pkgs.dockerTools.pullImage {
+              imageName = "";
+              imageDigest = "sha256:96e378d7e6531ac9a15ad505478fcc2e69f371b10f5cdf87857c4b8188404716";
+              hash = "sha256-REn1qvbn3hcNqdZsovUAkC+OuLFwglqp/WwPWMhiX6s=";
+              finalImageName = "debian";
+              finalImageTag = "bookworm-slim";
+            };
+
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = devdeps-todo-app ++ [
+                (pkgs.runCommand "bin-sh-link" { } ''
+                  mkdir -p $out/bin
+                  ln -s /usr/bin/dash $out/bin/sh
+                '')
+              ];
+              pathsToLink = [ "/bin" ];
+            };
+            config = {
+              Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+            };
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.gleam-overlay.overlays.default
+              inputs.gleam2nix.overlays.default
+	      inputs.fenix.overlays.default
+            ];
+            config = { };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                treefmt.enable = true;
+                gitleaks = {
+                  enable = true;
+                  entry = "${pkgs.gitleaks}/bin/gitleaks protect --staged";
+                  language = "system";
+                };
+                gitlint.enable = true;
+              };
+            };
+          };
+
+          process-compose."default-service" = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+            ];
+
+            services = {
+              # redis."r1" = {
+              #   enable = true;
+              # };
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            # To start the service, please run: nix run .#default-service
+            inputsFrom = [
+              config.process-compose."default-service".services.outputs.devShell
+            ];
+
+            packages = with pkgs; [
+	      nixd 
+	      just
+
+	      atlas
+	      sqlite
+	      sqlitebrowser
+            ] ++ erlangPackages
+            ++ gleamPackages ;
+
+	    ESQLITE_USE_SYSTEM=1;
+          };
+
+	  packages.default = app;
+	  packages.dockerImage = devShellImage;
+        };
+    };
+}
